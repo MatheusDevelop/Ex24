da820d113faf4e687f8b6105c78b7e226eab4b9d
Matheus Barbosa <mathvierabarbosa@gmail.com>
2020-06-25 18:25:10 -0300

    subindo

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ex24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tribonnaci conta = new Tribonnaci(10);
+            
+        }
+    }
+}
